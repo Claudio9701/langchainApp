f7c8cb5258be5c7301d89ccffb355032a229408d
Claudio <claudio@claudios-macbook-pro.home>
2023-07-11 11:18:53 +0200

    hotfix to makemigrations of main app too

--- runserver.sh
+++ runserver.sh
@@ -2,5 +2,6 @@
 
 python manage.py collectstatic --no-input
 python manage.py makemigrations
+python manage.py makemigrations main # Hotfix for makemigrations error
 python manage.py migrate
 gunicorn langchainApp.wsgi --bind=0.0.0.0:80

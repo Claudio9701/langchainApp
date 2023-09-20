fa810e9f119df3d248792be54dfab2ffa318e359
Claudio Ortega <claudio.rtega2701@gmail.com>
2023-09-20 12:02:24 +0200

    Set timeout to 300

--- runserver.sh
+++ runserver.sh
@@ -4,4 +4,4 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py makemigrations main # Hotfix for makemigrations error
 python manage.py migrate
-gunicorn langchainApp.wsgi --bind=0.0.0.0:80
+gunicorn langchainApp.wsgi --bind=0.0.0.0:80 --timeout 300
